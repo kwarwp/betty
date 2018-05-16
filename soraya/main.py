a9fa1568e429150a4972a5af379db24bfb631efc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-16 16:28:44 -0300

    Saved soraya/main.py at Timestamp: 2018-05-16 19:28:43

--- soraya/main.py
+++ soraya/main.py
@@ -1,1 +1,8 @@
-# betty.soraya.main.py
+from _spy.vittolino.main import Cena, Elemento, Texto
+
+#link da Sailor: https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjssv2J-IrbAhWO11MKHcrgDGcQjRx6BAgBEAU&url=https%3A%2F%2Fwww.rightstufanime.com%2FSailor-Moon-Crystal-Figure&psig=AOvVaw0GZQVycSDtbNe6fR1cKxMv&ust=1526584362925530
+linkDoCastelo="https://www.cpp.org.br/cultura/dica-cultural/item/10646-memorial-recria-castelo-ra-tim-bum"          
+
+def Historia ():
+    cenaCastelo = Cena (img = "https://www.cpp.org.br/cultura/dica-cultural/item/10646-memorial-recria-castelo-ra-tim-bum"
+    
