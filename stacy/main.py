a2d4056dfe7c387479d4bc7e9dd42b0cffaa866c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-07 11:29:06 -0300

    Created stacy/main.py at Timestamp: 2018-06-07 14:29:06

---
+++ stacy/main.py
@@ -0,0 +1,25 @@
+# betty.stacy.main.py
+class Templo:
+    def __init__(self):
+        self._esta_no_templo = False
+    def entra(self):
+        self._esta_no_templo = True
+    def sai(self):
+        self.sai = False
+        
+    def entrou(self):
+        return self._esta_no_templo
+        
+class EntradaDoTemplo(Templo):
+    def __init__(self):
+        super().__init__()
+    
+musica = Templo()
+oceano = Templo()
+floresta = Templo()
+floresta.entra()
+oceano.entra()
+entrada = EntradaDoTemplo()
+entrada.entra()
+print("musica:{},oceano:{},floresta:{}, entrada:{}".format(musica.entrou(),oceano.entrou(),
+             floresta.entrou(), entrada.entrou()))
