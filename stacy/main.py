1a69ade011804a42a334122f03b68fc62a1ccd77
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-07 12:03:16 -0300

    Saved stacy/main.py at Timestamp: 2018-06-07 15:03:16

--- stacy/main.py
+++ stacy/main.py
@@ -13,13 +13,27 @@ class Templo:
 class EntradaDoTemplo(Templo):
     def __init__(self):
         super().__init__()
+        self.corredor = CorredorDoTemplo()
+    def sai(self):
+        Templo.sai(self)
+        self.corredor.entra()
+        
+class CorredorDoTemplo(Templo):
+    def __init__(self):
+        super().__init__()
     
-musica = Templo()
-oceano = Templo()
-floresta = Templo()
-floresta.entra()
-oceano.entra()
+class SalaDoTemplo(CorredorDoTemplo):
+    def __init__(self):
+        super().__init__()
+    
+musica = SalaDoTemplo()
+oceano = SalaDoTemplo()
+floresta = SalaDoTemplo()
+#floresta.entra()
+#oceano.entra()
 entrada = EntradaDoTemplo()
+corredor = CorredorDoTemplo()
+corredor = entrada.corredor
 entrada.entra()
-print("musica:{},oceano:{},floresta:{}, entrada:{}".format(musica.entrou(),oceano.entrou(),
-             floresta.entrou(), entrada.entrou()))
+print("musica:{},oceano:{},floresta:{}, entrada:{}, corredor:{}".format(musica.entrou(),oceano.entrou(),
+             floresta.entrou(), entrada.entrou(), corredor.entrou()))
