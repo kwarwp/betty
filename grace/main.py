a3149f35497c425ace3852dc64317c5b28994941
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-20 17:57:04 -0300

    Created grace/main.py at Timestamp: 2018-06-20 20:57:04

---
+++ grace/main.py
@@ -0,0 +1,34 @@
+# betty.grace.main.py
+from _spy.vitollino.main import Cena, Texto, Elemento
+from _spy.vitollino.main import INVENTARIO as inv
+from soraya.main import Bloco
+from browser import alert
+
+CARINHAF ="https://i.imgur.com/n6b2S6t.png"
+ALIMENTO1 = "https://i.imgur.com/kUlL42h.jpg"
+ALIMENTO2 = "https://i.imgur.com/wPCVCa4.jpg"
+DESCANSO1 =  "https://i.imgur.com/kALoNiv.jpg"
+DESCANSO2 = "https://i.imgur.com/uLJYxzK.jpg"
+DIVERSAO1 = "https://i.imgur.com/qXk60ff.jpg"
+DIVERSAO2 ="https://i.imgur.com/PkJbh6T.jpg"
+EXCRMENTO1 = "https://i.imgur.com/6bhgxtG.jpg"
+EXCREMENTO2 = "https://i.imgur.com/kgnZgVq.jpg"
+FELIZ1 =  "https://i.imgur.com/fADJqIx.jpg"
+FELIZ2 ="https://i.imgur.com/s60ZiT1.jpg"
+FOME1 ="https://i.imgur.com/s8IfPKT.jpg"
+FOME2 ="https://i.imgur.com/xbWe7BC.jpg"
+SONO1 = "https://i.imgur.com/aAiB0rs.jpg"
+SONO2 ="https://i.imgur.com/DF0UOvo.jpg"
+SORRISOMAIS1 = "https://i.imgur.com/WCWBdwU.jpg"
+SORRISOMAIS2 = "https://i.imgur.com/xZ5mpcS.jpg"
+SORRISO1 = "https://i.imgur.com/M4LKzQL.jpg"
+SORRISO2 = "https://i.imgur.com/XpVabA8.jpg"
+
+def Robotica ():
+    sono1 = Cena(img=SONO1)
+    sono1.vai()
+    
+
+Robotica()
+
+
