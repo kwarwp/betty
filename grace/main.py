7d4b15ffd0d694d6b60383219220a3bf9180122e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-24 01:21:36 -0300

    Saved grace/main.py at Timestamp: 2018-06-24 04:21:36

--- grace/main.py
+++ grace/main.py
@@ -1,10 +1,8 @@
 # betty.grace.main.py
-from _spy.vitollino.main import Cena, Texto, Elemento
-from _spy.vitollino.main import INVENTARIO as inv
+from _spy.vitollino.main import Cena, Texto, Elemento, STYLE, INVENTARIO
 #colocar codog soraya.main e import bloco
-from browser import alert
-STYLE["width"] = "600px"
-STYLE["height"] = "600px"
+from browser import document, alert, html
+STYLE["width"]=900
 
 CARINHAF ="https://i.imgur.com/n6b2S6t.png"
 ALIMENTO1 = "https://i.imgur.com/kUlL42h.jpg"
@@ -33,13 +31,33 @@ TABELAFASE1 = "https://i.imgur.com/EMLDBhH.jpg"
 TABELAFASE2 = "https://i.imgur.com/yaeq5Ua.jpg"
 tabelafase1 = Cena(img=TABELAFASE1)
     
+
 def faseum ():
     tabelafase1 = Cena(img=TABELAFASE1)
-    tabelafase1.vai()
-    
+    tabelafase1.vai()    
 
 faseum()
 
-
+class Baralho ():
+    def __init__ (self, bloco, left=0, top=0, ileft=0, itop=0,
+                 size=dict(width=100, height=100)):
+        self.suporte = None
+        w, h = size.values()
+        self.fid = fid = "baralho_{}_{}".format(ileft, itop)
+        self.casa = "casa_{}_{}".format(left, top)
+        ileft, itop = "%dpx" % (-ileft*w), "%dpx" % (-itop*h)
+        style = {'position': 'absolute', 'overflow': 'hidden',
+                'background-image': 'url({})'.format(bloco.img),
+                'background-position': '{} {}'.format(ileft, itop),
+                'background-size': '{}px {}px'.format(*bloco.size),
+        }
+        style.update({k:'{}px'.format(v) for k, v in size.items() })
+        style.update(left="%dpx" % (left*(w+10)), top="%dpx" % (top*(h+10)))
+        self.folha = html.DIV(Id=fid, style=style, draggable=True)        
+        bloco.folha <= self.folha
+        self.folha.ondragstart = self.drag_start
+        self.folha.onmouseover = self.mouse_over
+        bloco.folhas[fid]=self
+        #INVENTARIO.score(casa=self.casa, carta=self.fid, move="INIT", ponto=0, valor=0)
 
 
