c1bfbca22adaeb9e0c5c800623c2040c477fb7fe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-11 17:05:51 -0300

    Saved grace/__log__.py at Timestamp: 2018-07-11 20:05:51

--- grace/__log__.py
+++ grace/__log__.py
@@ -112,3 +112,21 @@ AttributeError: 'Tabuleiro' object has no attribute 'tab0'
     self.lista_de_cartas[carta] = Elemento (carta, tit= carta, style=dict(
 AttributeError: 'Tabuleiro' object has no attribute 'lista_de_cartas'
 '''},
+{'date': 'Wed Jul 11 2018 17:02:55.867 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 80
+    Tabuleiro()
+  module <module> line 51
+    self.lista_de_cartas[carta] = Elemento (carta, tit= carta, style=dict(
+AttributeError: 'Tabuleiro' object has no attribute 'lista_de_cartas'
+'''},
