08326adf6b808f6d17d836e9c8feaae785fdaa64
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-11 16:11:01 -0300

    Saved grace/__log__.py at Timestamp: 2018-07-11 19:11:01

--- grace/__log__.py
+++ grace/__log__.py
@@ -76,3 +76,21 @@ SyntaxError: invalid syntax
     self.casa0.entra(tabelafase1)
 AttributeError: 'Tabuleiro' object has no attribute 'casa0'
 '''},
+{'date': 'Wed Jul 11 2018 16:08:06.26 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 76
+    Tabuleiro()
+  module <module> line 63
+    self.tabuleiro = {'0_0':self.tab0, '0_1':self.tab}
+AttributeError: 'Tabuleiro' object has no attribute 'tab0'
+'''},
