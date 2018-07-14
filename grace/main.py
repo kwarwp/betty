7880a547dfceacc8bf3189d1369892ec216cec99
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-13 23:17:39 -0300

    Saved grace/main.py at Timestamp: 2018-07-14 02:17:39

--- grace/main.py
+++ grace/main.py
@@ -8,12 +8,18 @@ FUNDO_BRANCO = "https://i.imgur.com/92bebxa.jpg"
 FUNDO_BRANCO_TESTE ="https://i.imgur.com/BxBeCrY.jpg"
 CARINHAF ="https://i.imgur.com/n6b2S6t.png"
 ALIMENTO2 = "https://i.imgur.com/kUlL42h.jpg"
-ALIMENTO1 = "https://i.imgur.com/wPCVCa4.jpg"
-DESCANSO1 =  "https://i.imgur.com/kALoNiv.jpg"
-DESCANSO2 = "https://i.imgur.com/uLJYxzK.jpg"
-DIVERSAO1 = "https://i.imgur.com/qXk60ff.jpg"
-DIVERSAO2 ="https://i.imgur.com/PkJbh6T.jpg"
-EXCREMENTO1 = "https://i.imgur.com/6bhgxtG.jpg"
+ALIMENTO_FASE1_1 = "https://i.imgur.com/C2sTazM.jpg"
+ALIMENTO_FASE1_5 = "https://i.imgur.com/i8u03BA.jpg"
+ALIMENTO_FASE1_9 = "https://i.imgur.com/gn9cGsu.jpg"
+DESCANSO_FASE1_2 =  "https://i.imgur.com/ruxtdF1.jpg"
+DESCANSO_FASE1_6 = "https://i.imgur.com/CBYMr6w.jpg"
+DESCANSO_FASE1_10 = "https://i.imgur.com/8Ufyps0.jpg"
+DIVERSAO_FASE1_3 = "https://i.imgur.com/usgSqOU.jpg"
+DIVERSAO_FASE1_7 = "https://i.imgur.com/bKn7iba.jpg"
+DIVERSAO_FASE1_11 = "https://i.imgur.com/8jJkqbp.jpg"
+EXCREMENTO_FASE1_4 = "https://i.imgur.com/RqQOALU.jpg"
+EXCREMENTO_FASE1_8 = "https://i.imgur.com/CH5iGGZ.jpg"
+EXCREMENTO_FASE1_12 = "https://i.imgur.com/jopwq9M.jpg"
 EXCREMENTO2 = "https://i.imgur.com/kgnZgVq.jpg"
 FELIZ1 =  "https://i.imgur.com/fADJqIx.jpg"
 FELIZ2 ="https://i.imgur.com/s60ZiT1.jpg"
@@ -21,13 +27,14 @@ FOME1 ="https://i.imgur.com/s8IfPKT.jpg"
 FOME2 ="https://i.imgur.com/xbWe7BC.jpg"
 SONO1 = "https://i.imgur.com/aAiB0rs.jpg"
 SONO2 ="https://i.imgur.com/DF0UOvo.jpg"
-SORRISOMAIS1 = "https://i.imgur.com/WCWBdwU.jpg"
+RESP_SORRISOMAIS_FASE1 = "https://i.imgur.com/kjQFRv8.jpg"
+RESP_SORRISO_FASE1 = "https://i.imgur.com/r5ZovCe.jpg"
+RESP_TRISTE_FASE1 = "https://i.imgur.com/CbrmJpE.jpg"
 SORRISOMAIS2 = "https://i.imgur.com/xZ5mpcS.jpg"
-SORRISO1 = "https://i.imgur.com/M4LKzQL.jpg"
 SORRISO2 = "https://i.imgur.com/XpVabA8.jpg"
 SUJO1 = "https://i.imgur.com/A64jbtF.jpg"
 SUJO2 = "https://i.imgur.com/5Vfq64q.jpg"
-TRISTE1 = "https://i.imgur.com/RslYMn9.jpg"
+TRISTE1 = "https://i.imgur.com/CbrmJpE.jpg"
 TRISTE2 = "https://i.imgur.com/PzDcx3q.jpg"
 TABELAFASE1 = "https://i.imgur.com/sp2gLBu.jpg"
 TABELAFASE2 = "https://i.imgur.com/SYkuH9o.jpg"
@@ -47,16 +54,16 @@ class Tabuleiro:
             carta_a_mover.elt.style.top = elemento_casa_do_tabuleiro.style.top
             print(elemento_casa_do_tabuleiro.style.left, elemento_casa_do_tabuleiro.style.top)
             print(carta_a_mover.elt.style.left, carta_a_mover.elt.style.top)
-            dica_do_valor = Elemento(SORRISOMAIS1, style=dict(left=950))
+            dica_do_valor = Elemento(RESP_SORRISOMAIS_FASE1, style=dict(left=950))
             dica_do_valor.entra(self.tabela_fase1)
         
         self.tabela_fase1 = tabelafase1 = Cena(img=TABELAFASE1)
-        self.pilha = Elemento(DESCANSO1, tit='descanso1', style=dict(
+        self.pilha = Elemento(ALIMENTO_FASE1_1, tit='Alimento1', style=dict(
             width="120px", height="90px", left=10, top=10))
             
         self.lista_de_cartas =[]
-        Pilha_Cartas = [ALIMENTO1, DESCANSO1, DIVERSAO1, EXCREMENTO1]
-        Resposta_Cartas = [(ALIMENTO1,"0_1","0_2 0_3 0_0"), DESCANSO1, DIVERSAO1, EXCREMENTO1]
+        Pilha_Cartas = [ALIMENTO_FASE1_1, DESCANSO_FASE1_2, DIVERSAO_FASE1_3, EXCREMENTO_FASE1_4]
+        Resposta_Cartas = [(ALIMENTO_FASE1_1,"0_1","0_2 0_3 0_0"), DESCANSO_FASE1_2, DIVERSAO_FASE1_3, EXCREMENTO_FASE1_4]
         
         
         #self.pilha0.vai = move_carta
@@ -96,7 +103,7 @@ class Tabuleiro:
         for coluna in range(4):
             for linha in range(4):
                 nome = "{}_{}".format(linha, coluna)
-                self.tabuleiro[nome] = Elemento(FUNDO_BRANCO_TESTE, tit=nome+"_", style=dict(
+                self.tabuleiro[nome] = Elemento(FUNDO_BRANCO, tit=nome+"_", style=dict(
                     width=TBX-11, height="{}px".format(TBY-52), left=inicio_x+coluna*TBX, top=inicio_y+linha*TBY))
                 self.tabuleiro[nome].entra(tabelafase1)
                 self.tabuleiro[nome].img.id = nome
