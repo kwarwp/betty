86a306a0160929a0e129674cff44200ffb192504
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-15 12:23:02 -0300

    Saved julia/__log__.py at Timestamp: 2018-08-15 15:23:02

--- julia/__log__.py
+++ julia/__log__.py
@@ -31,3 +31,19 @@ AttributeError: 'Cena' object has no attribute 'norte'
     primeira_cena.meio = segunda_cena.leste
 AttributeError: 'Cena' object has no attribute 'leste'
 '''},
+{'date': 'Wed Aug 15 2018 12:22:59.369 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 7
+    primeira_cena.leste.meio = segunda_cena
+AttributeError: 'Cena' object has no attribute 'leste'
+'''},
