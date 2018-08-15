a7627df3461a5dfa7368c19890a1cc4eeb56bea1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-15 18:15:26 -0300

    Saved julia/__log__.py at Timestamp: 2018-08-15 21:15:25

--- julia/__log__.py
+++ julia/__log__.py
@@ -79,3 +79,19 @@ ImportError: _spy
     from _spy.vitollino.main import Cena
 ImportError: _spy
 '''},
+{'date': 'Wed Aug 15 2018 21:15:23.491 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 10
+    sala.vai()
+AttributeError: 'Sala' object has no attribute 'vai'
+'''},
