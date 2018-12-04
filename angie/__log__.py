b973778d31543f4cdb55627388cd26a02e9862e8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-12-04 15:38:09 -0200

    Created angie/__log__.py at Timestamp: 2018-12-04 17:38:08

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Dec 04 2018 15:38:13.697 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  #circus(<ponha aqui o número do desafio e descomente a linha>, <parâmetro indicado>)
+                                                                                      ^
+IndentationError: expected an indented block
+'''},
