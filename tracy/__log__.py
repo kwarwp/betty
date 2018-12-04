71e846a4766bd069dd1e14fbdd7481933555d886
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-12-04 16:39:39 -0200

    Saved tracy/__log__.py at Timestamp: 2018-12-04 18:39:39

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -36,3 +36,10 @@ TypeError: 'module' object is not callable
           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Dec 04 2018 16:39:46.221 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  circus(<1>)
+          ^
+SyntaxError: invalid syntax
+'''},
