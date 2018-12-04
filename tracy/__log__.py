4652cc02897824a1914ffba6a2e04a859a1523e0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-12-04 16:34:47 -0200

    Created tracy/__log__.py at Timestamp: 2018-12-04 18:34:47

---
+++ tracy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Dec 04 2018 16:34:52.91 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  TOPO_ESQUERDA = "AN"
+  ^
+IndentationError: expected an indented block
+'''},
