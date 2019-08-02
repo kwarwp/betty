de94b15337bbfb310f889fae37e676c80d3c7967
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-01 23:02:22 -0300

    Created soraya/__log__.py at Timestamp: 2019-08-02 02:02:22

---
+++ soraya/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Aug 01 2019 23:03:56.134 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  cenaCastelo = Cena (img = "https://www.cpp.org.br/cultura/dica-cultural/item/10646-memorial-recria-castelo-ra-tim-bum"
+                     ^
+SyntaxError: Unbalanced bracket (
+'''},
