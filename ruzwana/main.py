78851d383e0dbc0e1dae9b131728a757b3fdd4bd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-08 18:28:32 -0300

    Saved ruzwana/main.py at Timestamp: 2019-08-08 21:28:31

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -7,8 +7,24 @@ class Agenda:
         self.senha = input("entre a senha")
         while self.senha != "ratimbum":
             self.senha = input("entre a senha")
+        self.menu()
+            
+    def menu(self, info=""):
+        self.acao = input(f"{info}digite 1 para consulta ou 2 para novo nome")
+        if self.acao == "1":
+            self.consulta_numero()
+        else:
+            self.novo_numero()
+            
+    def novo_numero(self):
         self.nome = input("entre o nome")
         self.tel = input("entre o telefone")
         self.agenda.update({self.nome: self.tel})
+        self.menu()
+            
+    def consulta_numero(self):
+        self.nome = input("entre o nome")
+        self.tel = self.agenda[self.nome]
+        self.menu(f"O telefone de {self.nome} é {self.tel}\n")
         
 Agenda()
