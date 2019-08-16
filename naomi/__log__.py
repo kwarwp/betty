376224dc7fcb694fc8ddfadd16d3dbc36d7bc706
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-16 15:35:36 -0300

    Saved naomi/__log__.py at Timestamp: 2019-08-16 18:35:36

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -87,3 +87,25 @@ KeyError: img
     imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
 KeyError: img
 '''},
+{'date': 'Fri Aug 16 2019 15:35:35.389 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 7
+    minions()
+  module <module> line 5
+    	cena = Cena (img = bananeira, style=dict(left=150, top=60, width=100, height=200))
+  module _spy.vitollino.main line 782
+    Cena.c(**kwargs)
+  module _spy.vitollino.main line 825
+    imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
+KeyError: img
+'''},
