9cba00bad40ffe9cf1505f4611b02fe16bc3c7ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-16 15:08:59 -0300

    Saved naomi/main.py at Timestamp: 2019-08-16 18:08:59

--- naomi/main.py
+++ naomi/main.py
@@ -2,6 +2,6 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 bananeira = "https://bykamy.com.br/media/magpleasure/mpblog/upload/a/e/ae69211a1a37d81e6472903dca1ff6c7.jpg"
 def minions():
-	cena = Cena (img = bananeira)
+	cena = Cena (img = bananeira, width=100, height=300)
 	cena.vai()
 minions()
