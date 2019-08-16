01b887d457372b1802303d39d5f78c723331b323
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-16 15:04:50 -0300

    Created naomi/main.py at Timestamp: 2019-08-16 18:04:49

---
+++ naomi/main.py
@@ -0,0 +1,6 @@
+# betty.naomi.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto
+bananeira = "https://bykamy.com.br/media/magpleasure/mpblog/upload/a/e/ae69211a1a37d81e6472903dca1ff6c7.jpg"
+def minions():
+	cena = Cena (img = bananeira)
+    
