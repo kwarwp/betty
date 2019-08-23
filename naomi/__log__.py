c4c1ebe4e54af029ef38dfd152c4e5839e3cb416
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-23 14:41:20 -0300

    Saved naomi/__log__.py at Timestamp: 2019-08-23 17:41:19

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -166,3 +166,25 @@ SyntaxError: invalid syntax
     imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
 KeyError: img
 '''},
+{'date': 'Fri Aug 23 2019 14:41:23.301 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 11
+    minions()
+  module <module> line 7
+    	cena = Cena (img = bananeira,style=dict( width=500,height=500))
+  module _spy.vitollino.main line 782
+    Cena.c(**kwargs)
+  module _spy.vitollino.main line 825
+    imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
+KeyError: img
+'''},
