03b8fc21d7b99734630ff3481d77e9875022fad4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-30 14:34:23 -0300

    Saved naomi/__log__.py at Timestamp: 2019-08-30 17:34:24

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -286,3 +286,10 @@ SyntaxError: invalid syntax
                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Aug 30 2019 14:34:24.659 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  banana = imgu "https://i.r.com/VbszxUx.png"
+                 ^
+SyntaxError: invalid syntax
+'''},
