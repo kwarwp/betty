717a45e77be7e7e01d381efdcafd16ed6801a6bd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-30 14:00:46 -0300

    Saved naomi/main.py at Timestamp: 2019-08-30 17:00:46

--- naomi/main.py
+++ naomi/main.py
@@ -1,13 +1,15 @@
 # betty.naomi.main.py
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 bananeira = "https://bykamy.com.br/media/magpleasure/mpblog/upload/a/e/ae69211a1a37d81e6472903dca1ff6c7.jpg"
-banana = "https://i.imgur.com/VbszxUx.png"
+banana = "https://i.imgur.com/VbszxUx.png
 STYLE["width"]=1100
 STYLE["height"]="600px"
 
 def minions():
 	cena = Cena (img = bananeira)
-	elemento = Elemento(img = banana)
+	elemento = Elemento(img = banana ,
+                         tit="banana",
+                         style=dict(left=150, top=60, width=60, height=200))
 	elemento.entra(cena)
 	cena.vai()
 minions()
