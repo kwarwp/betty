5f08ca76b03ebff0dc45fae803f7b5476b80fa17
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-30 14:02:11 -0300

    Saved naomi/main.py at Timestamp: 2019-08-30 17:02:11

--- naomi/main.py
+++ naomi/main.py
@@ -1,7 +1,7 @@
 # betty.naomi.main.py
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 bananeira = "https://bykamy.com.br/media/magpleasure/mpblog/upload/a/e/ae69211a1a37d81e6472903dca1ff6c7.jpg"
-banana = "https://i.imgur.com/VbszxUx.png
+banana = "https://i.imgur.com/VbszxUx.png"
 STYLE["width"]=1100
 STYLE["height"]="600px"
 def minions():
