d565b4e0b0867a118e72ca2abd889f9d64acbaa0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-30 15:39:35 -0300

    Saved naomi/main.py at Timestamp: 2019-08-30 18:39:34

--- naomi/main.py
+++ naomi/main.py
@@ -15,14 +15,14 @@ def minions():
                          style=dict(left=320 , top=250, width="100px", height="120px"))
 	elemento1 = Elemento(img = banana1 ,
                          tit="banana1",
-                         style=dict(left=400, top=250, width="100px", height="120px"))                      
+                         style=dict(left=400, top=250, width="90px", height="110px"))                      
 	elemento.entra(cena)
 	elemento2 = Elemento(img = banana ,
                          tit="banana2",
-                         style=dict(left=100 , top=250, width="80px", height="100px"))
+                         style=dict(left=40 , top=300, width="80px", height="100px"))
 	elemento3 = Elemento(img = banana ,
                          tit="banana3",
-                         style=dict(left=200, top=250, width="80px", height="100px"))
+                         style=dict(left=150, top=300, width="60px", height="80px"))
 	elemento1.entra(cena)
 	elemento2.entra(cena)
 	elemento3.entra(cena)
