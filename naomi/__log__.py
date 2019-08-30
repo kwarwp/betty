9a5b3092c49abf17be9f5e4fe0947bc1368a57b3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-30 15:06:15 -0300

    Saved naomi/__log__.py at Timestamp: 2019-08-30 18:06:15

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -368,3 +368,21 @@ NameError: name 'Elemento1' is not defined
                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Aug 30 2019 15:06:13.975 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 25
+    minions()
+  module <module> line 23
+    	elemento2.entra(cena)    
+NameError: name 'elemento2' is not defined
+'''},
