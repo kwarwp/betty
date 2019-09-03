b8c9f596c9fa87b538d8ea0743c154e3d913b8b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-03 15:03:17 -0300

    Created lisa/__log__.py at Timestamp: 2019-09-03 18:03:17

---
+++ lisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Sep 03 2019 15:03:55.151 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from_spy.vitollino.import principal Cena, Texto, Elemento
+                      ^
+SyntaxError: invalid syntax
+'''},
