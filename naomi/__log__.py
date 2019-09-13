cd99bf58d6b2a4b4fbf5d1272838ace85d8bc819
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-13 15:09:19 -0300

    Saved naomi/__log__.py at Timestamp: 2019-09-13 18:09:19

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -467,3 +467,10 @@ SyntaxError: invalid syntax
                                                                                                                                                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Sep 13 2019 15:09:20.368 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 31
+  elemento5 = Elemento(img = caminhao ,
+                                                                                                                                                               ^
+SyntaxError: invalid syntax
+'''},
