3d45d97641fad8562e88f09e40b6aa2149e2be96
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-13 15:18:42 -0300

    Saved naomi/__log__.py at Timestamp: 2019-09-13 18:18:41

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -495,3 +495,10 @@ SyntaxError: EOL while scanning string literal
                                                                                                                                                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Sep 13 2019 15:18:42.732 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 30
+  elemento5 = Elemento(img = caminhao ,
+                                                                                                                                                               ^
+SyntaxError: invalid syntax
+'''},
