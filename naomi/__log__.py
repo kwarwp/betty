b18d7e994d3d67479b70a9ac50398f312eb698ee
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-01 16:01:58 -0300

    Saved naomi/__log__.py @2019-10-01 19:01

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -807,3 +807,21 @@ UnboundLocalError: local variable 'podre1' referenced before assignment
     	Podre1 = Elemento(img = podre1 ,
 NameError: name 'podre1' is not defined
 '''},
+{'date': 'Tue Oct 01 2019 16:01:56.510 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 60
+    Incrivel_banana_python()
+  module <module> line 58
+    	podre1.entra(cena)
+AttributeError: 'str' object has no attribute 'entra'
+'''},
