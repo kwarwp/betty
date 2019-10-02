84e2e170a3662e4be62bd433acb06827606b9664
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 09:19:49 -0300

    Saved morgan/__log__.py @2019-10-02 12:19

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Oct 02 2019 09:19:44.633 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  elemento = Elemento (img = carro
+                                     ^
+SyntaxError: invalid syntax
+'''},
