f08516c980bf024e6f2a97116735f87b23c5a711
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 10:54:07 -0300

    Saved naomi/main.py @2019-10-02 13:54

--- naomi/main.py
+++ naomi/main.py
@@ -10,18 +10,21 @@ caminhao = "https://i.imgur.com/M62FANW.png"
 geleia = "https://static.extratoverde.com.br/public/extratoverde/imagens/produtos/geleia-organica-zero-acucar-sabor-banana-shambala-240g-2457.png"
 podre = "https://i.imgur.com/sGUZfwF.png"
 lixeira = "https://cdn.pixabay.com/photo/2012/04/24/16/34/garbage-40357_960_720.png"
-podre17 = "https://i.imgur.com/sGUZfwF.png"
+podre1 = "https://i.imgur.com/sGUZfwF.png"
 STYLE["width"]=1100
 STYLE["height"]="600px"
+DICIONARIO_CAMINHAO = {}
+BOAS = "banana banana1 banana2 banana3 banana4".split()
+
 def Incrivel_banana_python():
-	cena = Cena (img = bananeira)
+	cenario = Cena (img = bananeira)
 	elemento = Elemento(img = banana ,
                          tit="banana",
                          style=dict(left=320 , top=250, width="100px", height="120px"))
 	elemento1 = Elemento(img = banana1 ,
                          tit="banana1",
                          style=dict(left=400, top=250, width="90px", height="110px"))                      
-	elemento.entra(cena)
+	elemento.entra(cenario)
 	elemento2 = Elemento(img = banana ,
                          tit="banana2",
                          style=dict(left=40 , top=250, width="80px", height="90px"))
@@ -44,17 +47,21 @@ def Incrivel_banana_python():
 	Lixeira = Elemento(img = lixeira ,
                         tit= "lixeira", 
                         style=dict(left=200,top=420,width="200px",heigth="250px"))
-	podre1 = Elemento(img = podre1 ,
+	"""podre1 = Elemento(img = podre1 ,
                         tit= "podre1",
-                        style=dict(left=150,top=400,width="100px",heigth="250px"))
-	elemento1.entra(cena)
-	elemento2.entra(cena)
-	elemento3.entra(cena)
-	elemento4.entra(cena)
-	Lixeira.entra(cena)
-	Caminhao.entra(cena)
-	Geleia.entra(cena)
-	Podre.entra(cena)
-	podre1.entra(cena)
-	cena.vai()
+                        style=dict(left=150,top=400,width="100px",heigth="250px"))"""
+	elemento1.entra(cenario)
+	elemento2.entra(cenario)
+	elemento3.entra(cenario)
+	elemento4.entra(cenario)
+	Lixeira.entra(cenario)
+	Caminhao.entra(cenario)
+	Geleia.entra(cenario)
+	Podre.entra(cenario)
+	"""podre1.entra(cenario)"""
+	cenario.vai()
+    
+def aceita_banana_boa (evento, nome)
+	Texto (cenario, "Muito bem, esta boa {nome} pode ser vendida!").vai() 
+    
 Incrivel_banana_python()
