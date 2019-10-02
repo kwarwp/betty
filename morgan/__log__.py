b61c4358b690694f63d1e4b3946ea3ece71ff81c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-02 08:55:00 -0300

    Created morgan/__log__.py @2019-10-02 11:55

---
+++ morgan/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Oct 02 2019 08:54:52.281 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  cidade = "http://www.trabalhosescolares.net/wp-content/uploads/2019/08/cidade_trabalhos_escolares.jpg"
+  ^
+IndentationError: unexpected indent
+'''},
