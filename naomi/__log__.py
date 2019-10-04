9c8507907f1f50fdeb628b7c1b99df07855410c5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-04 13:36:50 -0300

    Saved naomi/__log__.py @2019-10-04 16:36

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -935,3 +935,10 @@ SyntaxError: invalid syntax
                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Oct 04 2019 13:36:50.758 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 64
+  def aceita_banana_boas (evento, nome)
+                                        ^
+SyntaxError: invalid syntax
+'''},
