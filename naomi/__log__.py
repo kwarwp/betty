8a4576427285b2ebf3cffbe58060a3bef2eb21ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-04 13:33:11 -0300

    Saved naomi/__log__.py @2019-10-04 16:33

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -886,3 +886,10 @@ UnboundLocalError: local variable 'podre1' referenced before assignment
                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Oct 04 2019 13:33:12.85 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 64
+  def aceita_banana_boa (evento, nome)
+                                       ^
+SyntaxError: invalid syntax
+'''},
