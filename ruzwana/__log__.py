6e9197b18e8c2628b0110c77a30b33e3c4673661
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-05 17:25:21 -0300

    Saved ruzwana/__log__.py @2019-10-05 20:25

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -5,3 +5,25 @@ RuntimeError: too much recursion
   module '$exec_514' line 30
 Agenda()
 '''},
+{'date': 'Sat Oct 05 2019 17:24:09.618 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    Agenda()
+  module <module> line 10
+    self.menu()
+  module <module> line 15
+    self.consulta_numero()
+  module <module> line 27
+    self.tel = self.agenda[self.nome]
+KeyError: isa
+'''},
