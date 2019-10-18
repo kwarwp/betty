8d3c8895231be6d39555bad6f9c09e1bd8ef4807
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-18 14:27:39 -0300

    Saved naomi/__log__.py @2019-10-18 17:27

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -1163,3 +1163,21 @@ NameError: name 'banana' is not defined
     	elemento = Elemento(img = banana ,                        
 NameError: name 'banana' is not defined
 '''},
+{'date': 'Fri Oct 18 2019 14:27:37.238 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 103
+    Incrivel_banana_python()
+  module <module> line 37
+    	elemento3 = Elemento(img = banana ,
+NameError: name 'banana' is not defined
+'''},
