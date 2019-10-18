6b55dbe1bffc89a8ec3d83a3fa80fbae8c236836
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-18 10:19:11 -0300

    Saved naomi/__log__.py @2019-10-18 13:19

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -1045,3 +1045,21 @@ NameError: name 'Banana_madura1' is not defined
                                                                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Oct 18 2019 10:19:10.24 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 98
+    Incrivel_banana_python()
+  module <module> line 80
+    	deBanana_madura2.entra(cenario)
+NameError: name 'deBanana_madura2' is not defined
+'''},
