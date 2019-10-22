cbb9ffb7acd2867d8684cb8067e008108e4f66f5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-22 00:26:59 -0300

    Saved naomi/__log__.py @2019-10-22 03:26

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -1690,3 +1690,21 @@ UnboundLocalError: local variable 'cenario_jogo' referenced before assignment
     	cenario2.meio.esquerda.direita = cenario_jogo
 UnboundLocalError: local variable 'cenario_jogo' referenced before assignment
 '''},
+{'date': 'Tue Oct 22 2019 00:26:57.677 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 139
+    Incrivel_banana_python()
+  module <module> line 88
+    	cenario2.cenario.meio.esquerda.direita = cenario
+AttributeError: 'Cena' object has no attribute 'cenario'
+'''},
