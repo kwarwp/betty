c8e403fddf2b47f5c9eea433668e72c8307d95a8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-21 23:25:32 -0300

    Saved naomi/__log__.py @2019-10-22 02:25

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -1294,3 +1294,21 @@ NameError: name 'rejeita_banana_madura' is not defined
     	cenario2.vai()
 AttributeError: 'str' object has no attribute 'vai'
 '''},
+{'date': 'Mon Oct 21 2019 23:25:30.670 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 137
+    Incrivel_banana_python()
+  module <module> line 86
+    	cenario2.vai()
+AttributeError: 'str' object has no attribute 'vai'
+'''},
