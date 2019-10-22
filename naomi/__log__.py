f569389ba1ed3c3b5bd63387b7eb8bf39c96a8e0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-21 23:30:04 -0300

    Saved naomi/__log__.py @2019-10-22 02:30

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -1366,3 +1366,21 @@ NameError: name 'cnario2' is not defined
     	cnario2.meio=cenario
 NameError: name 'cnario2' is not defined
 '''},
+{'date': 'Mon Oct 21 2019 23:30:01.867 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 138
+    Incrivel_banana_python()
+  module <module> line 88
+    	cnario2.meio = cenario
+NameError: name 'cnario2' is not defined
+'''},
