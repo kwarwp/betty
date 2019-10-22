668ef1bdd39f8a9f678fa2c7e3263e4fca42aa29
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-22 00:41:49 -0300

    Saved naomi/main.py @2019-10-22 03:41

--- naomi/main.py
+++ naomi/main.py
@@ -85,7 +85,7 @@ def Incrivel_banana_python():
 	Banana_madura4.entra(cenario)   
     
 	cenario2.vai()
-	cenario2.meio = cenario2.cenario
+	cenario2.meio.direita.esquerda = cenario = Cena (img = bananeira)
 	cenario.vai()
     
 def aceita_banana_boa(evento, nome):
