ec0ae88b091fd865a3db1a1482f55baa89053c9c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-23 09:36:12 -0300

    Saved lisa/main.py @2019-10-23 12:36

--- lisa/main.py
+++ lisa/main.py
@@ -2,3 +2,8 @@
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 FUTURO = "https://www.ideiasdecor.com/wp-content/uploads/2018/09/plantas-de-casas-57.jpg"
 STYLE["width"]=1100
+
+def meu_futuro():
+	Cfuturistica = Cena (img FUTURO) 
+    
+	Cfuturo.vai() 
