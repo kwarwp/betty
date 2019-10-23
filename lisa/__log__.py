16ec7a848052db14183748ca5fc7636ea3129666
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-23 09:37:23 -0300

    Saved lisa/__log__.py @2019-10-23 12:37

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                             ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Oct 23 2019 09:37:22.471 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  Cfuturistica = Cena (img FUTURO) 
+                            ^
+SyntaxError: invalid syntax
+'''},
