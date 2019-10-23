854653f75cadddaa5adce505c2579e5546067c5e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-23 09:24:04 -0300

    Created lisa/main.py @2019-10-23 12:24

---
+++ lisa/main.py
@@ -0,0 +1,4 @@
+# betty.lisa.main.py
+from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
+FUTURO = "https://www.ideiasdecor.com/wp-content/uploads/2018/09/plantas-de-casas-57.jpg"
+STYLE["width"]=1100
