1d48388f90999a3f50e02439b1ccd41951d63bc3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-30 10:36:41 -0300

    Saved lisa/__log__.py @2019-10-30 13:36

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -71,3 +71,10 @@ NameError: name 'Cfuturistica' is not defined
                                                                                                                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Oct 30 2019 09:36:36.308 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  fulaninho = Elemento (img = fulaninho ,
+                                                                                                                 ^
+SyntaxError: invalid syntax
+'''},
