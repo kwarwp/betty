26d05d49406e4e058588a9bbd58f720b1d965b27
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-30 08:52:24 -0300

    Saved lisa/__log__.py @2019-10-30 11:52

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -41,3 +41,10 @@ SyntaxError: invalid syntax
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Oct 30 2019 08:52:22.277 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  Cfuturistica.vai()
+  ^
+IndentationError: unexpected indent
+'''},
