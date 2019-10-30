8d15bba923c6802496f6c32a11855d3cead5540d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-30 10:49:41 -0300

    Saved lisa/main.py @2019-10-30 13:49

--- lisa/main.py
+++ lisa/main.py
@@ -1,14 +1,14 @@
 # betty.lisa.main.py
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 FUTURO = "https://www.ideiasdecor.com/wp-content/uploads/2018/09/plantas-de-casas-57.jpg"
-FULANINHO= "https://www.pinpng.com/pngs/m/109-1091652_woman-gray-people-cutout-cut-out-people-people.png"
+FULANINHO= "https://pngimage.net/wp-content/uploads/2018/06/pessoa-em-p%C3%A9-png.png"
 STYLE["width"]=1100
 
 def meu_futuro():
 	Cfuturistica = Cena (img = FUTURO)
 	Cfuturistica.vai()
-	fulaninho = Elemento (img = FULANINHO ,
-                            tit= "mulher"
-                            style=dict(left=320 , top=250, width="100px", height="120px"))
+	fulaninho = Elemento (img = FULANINHO, tit= "mulher",
+                            style=dict(left=320 , top=250, width="1OOpx", height="120px"))
 	fulaninho.entra(Cfuturistica)    
 meu_futuro()
+
