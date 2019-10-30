56ec79948d90cae8b11ad74a75b60a1dd2f42e64
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-30 11:30:05 -0300

    Saved julia/main.py @2019-10-30 14:30

--- julia/main.py
+++ julia/main.py
@@ -1,1 +1,7 @@
 # betty.julia.main.py
+from  _spy.vitollino.main import Cena, Elemento, STYLE, Texto
+Espaco="https://hypescience.com/wp-content/uploads/2016/05/espaco-tres-dimensoes-3d.jpg"
+def sistemasolar():
+	espaco=Cena(img=Espaco)
+	espaco.vai()
+sistemasolar()    
