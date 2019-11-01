14e66eece8501aef6d6f5688b85e0c029779371a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 14:46:55 -0300

    Saved tracy/__log__.py @2019-11-01 17:46

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -281,3 +281,10 @@ SyntaxError: invalid syntax
                                                                                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Nov 01 2019 13:46:52.530 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 53
+  Lixeira = Elemento(img = lixeira , drop=DRAGGER_LIXEIRA,
+                                                                                                                                      ^
+SyntaxError: invalid syntax
+'''},
