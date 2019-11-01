a0e5c3b0d676e03cd4b211a9a86b2d670b9889aa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 14:17:48 -0300

    Saved tracy/__log__.py @2019-11-01 17:17

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -197,3 +197,10 @@ SyntaxError: invalid syntax
                                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Nov 01 2019 13:17:45.637 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 23
+  cenario2 = Cena(img = tela_inicial = esquerda=cenario)
+                                                        ^
+SyntaxError: invalid syntax
+'''},
