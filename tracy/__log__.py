9e34401f115d400918fc198fe77c2db08a3e1bb2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 14:57:27 -0300

    Saved tracy/__log__.py @2019-11-01 17:57

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -363,3 +363,10 @@ AttributeError: 'Cena' object has no attribute 'cenario'
                                                                                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Nov 01 2019 13:57:24.235 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 58
+  lixo = Elemento(img = lixo, drag=True
+                                                              ^
+SyntaxError: invalid syntax
+'''},
