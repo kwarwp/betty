f20818ee1958a9b62c5ac63b39a4c4d0c287d894
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 09:59:28 -0300

    Saved julia/main.py @2019-11-06 12:59

--- julia/main.py
+++ julia/main.py
@@ -1,7 +1,16 @@
 # betty.julia.main.py
 from  _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 Espaco="https://hypescience.com/wp-content/uploads/2016/05/espaco-tres-dimensoes-3d.jpg"
+nave="https://www.trzcacak.rs/myfile/detail/298-2980234_nave-espacial-png-3d-space-shuttle-metal-3d.png"
+STYLE["width"]=1100
+STYLE["height"]="600px"
 def sistemasolar():
 	espaco=Cena(img=Espaco)
 	espaco.vai()
+	elementonave=Elemento(img = nave, tit="nave", style=dict( left=320 , top=250, width="100px", height="120px"))
+	elementonave.entra(espaco)
+    
+    
+    
 sistemasolar()    
+
