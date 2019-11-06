b07e6a8f013903f180dccf4134bff17bc2267f0e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 10:47:49 -0300

    Saved soraya/__log__.py @2019-11-06 13:47

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -335,3 +335,10 @@ SyntaxError: EOL while scanning string literal
                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 06 2019 10:47:49.335 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  elementopessoa = Elemento(img = pessoa tit="banana", drag=True,
+                                          ^
+SyntaxError: invalid syntax
+'''},
