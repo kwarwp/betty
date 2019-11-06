3b5018e01c8b5fd9f2598bfbc4af82177f4400b6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 09:49:13 -0300

    Saved soraya/main.py @2019-11-06 12:49

--- soraya/main.py
+++ soraya/main.py
@@ -1,3 +1,7 @@
 # betty.soraya.main.py
 from _spy.vitollino.main import Cena, STYLE, Texto, Elemento
 praia = "http://baboonnomundao.com.br/bannersist/conteudo/BC_Floripa_-_SC.jpg"
+def lol ():
+	cenario = Cena (img = praia)
+	cenario.vai()
+lol()
