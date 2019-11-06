e77f2aef38e1e39e8d7648da3f88e82c3eb422b7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 10:39:11 -0300

    Saved soraya/main.py @2019-11-06 13:39

--- soraya/main.py
+++ soraya/main.py
@@ -1,7 +1,7 @@
 # betty.soraya.main.py
 from _spy.vitollino.main import Cena, STYLE, Texto, Elemento
 praia = "http://baboonnomundao.com.br/bannersist/conteudo/BC_Floripa_-_SC.jpg"
-pessoa = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Dilma_Rousseff_2010_Transparent.png/480px-Dilma_Rousseff_2010_Transparent.png
+pessoa = "https://upload.wikimedia.org/wikipedia/commons/9/93/Flamengo-RJ_%28BRA%29.png
 STYLE["width"]=1500
 STYLE["height"]="600px"
 
@@ -9,6 +9,7 @@ def lol():
 	Cenario = Cena(img = praia)
 	Cenario.vai()
 	elementopessoa = Elemento(img = pessoa tit="banana", drag=True,
-                         style=dict(left=320 , top=250, width="100px", height="120px"))  
+                         style=dict(left=320 , top=250, width="100px", height="120px")) 
+	elemento1.vai(png = pessoa)
     
 lol()
