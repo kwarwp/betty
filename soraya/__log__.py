c3b67f60977f92028201933c4e0959bd61390f9e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 11:13:26 -0300

    Saved soraya/__log__.py @2019-11-06 14:13

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -491,3 +491,21 @@ NameError: name 'elemento' is not defined
     	elemento.entra(cenario)
 NameError: name 'elemento' is not defined
 '''},
+{'date': 'Wed Nov 06 2019 11:13:25.482 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 13
+    lol()
+  module <module> line 12
+    	elemento.entra(cenario)
+NameError: name 'elemento' is not defined
+'''},
