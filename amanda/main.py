85a2f5d7b1c4e09577c9820d3ee700194b6f03f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-08 15:07:08 -0300

    Created amanda/main.py @2019-11-08 18:07

---
+++ amanda/main.py
@@ -0,0 +1,8 @@
+# betty.amanda.m
+from  _spy.vitollino.main import Cena, Elemento, Texto, STYLE
+NATUREZA="https://i0.wp.com/parqueibirapuera.org/wp/wp-content/uploads/2019/04/Aaapoll1.jpg?ssl=1"
+
+def natura():
+	natural=Cena (img=NATUREZA)
+	natural.vai()
+   
