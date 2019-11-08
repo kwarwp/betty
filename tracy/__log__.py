ec76211a41a7fe758e712cb8a71eb14e7a421ffb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-08 14:55:26 -0300

    Saved tracy/__log__.py @2019-11-08 17:55

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -791,3 +791,21 @@ AttributeError: 'Cena' object has no attribute 'cenario'
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Fri Nov 08 2019 14:55:25.587 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 101
+    Incrivel_banana_python()
+  module <module> line 73
+    	cenario2.esquerda = cenario2.cenario
+AttributeError: 'Cena' object has no attribute 'cenario'
+'''},
