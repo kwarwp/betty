82e5d1914f6a5dae5f9822882a4a8159aa78e843
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 11:06:24 -0300

    Saved samantha/main.py @2019-11-19 14:06

--- samantha/main.py
+++ samantha/main.py
@@ -3,6 +3,11 @@ from _spy.vitollino.main import Cena, Texto, Elemento, STYLE
 Namek='https://images3.alphacoders.com/677/thumb-1920-677271.png'
 STYLE['width']=1380
 STYLE['height']='605px'
+
 def dragonballz():
 	Cena_Namek= Cena(img=Namek)
-	Cena_Namek.vai
+	Cena_Namek.vai()
+    
+    
+    
+dragonballz()
