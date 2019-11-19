191f83ac34bd6b87341b5a5d65887211ab535dbf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 09:20:33 -0300

    Saved anastasia/main.py @2019-11-19 12:20

--- anastasia/main.py
+++ anastasia/main.py
@@ -2,6 +2,8 @@
 from _spy.vitollino.main import Cena, Texto, Elemento, STYLE 
 espaco="https://conhecimentocientifico.r7.com/wp-content/uploads/2017/02/destaque.jpg"
 ESPACO= Cena ( img=espaco)
+STYLE["width"]=1100
+STYLE["height"]="800px"
 
 def funciona():
 	ESPACO.vai()
