e0455f0cc3116cc275594266b12546ad709bf669
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 10:21:20 -0300

    Saved kristen/main.py @2019-11-19 13:21

--- kristen/main.py
+++ kristen/main.py
@@ -1,11 +1,14 @@
 # betty.kristen.main.py
 from _spy.vitollino.main import Cena, Texto, Elemento, STYLE
 Campo='https://turismo.buenosaires.gob.ar/sites/turismo/files/campo_de_golf_1200_c.jpg'
-
-STYLE['width']=1380
+Dogin='http://3.bp.blogspot.com/-4A5-CTXo8Jk/VUqmCKVdwfI/AAAAAAAAzzI/aiYW51qUD-s/s1600/dog_PNG147.png'
+STYLE['width']=1350
 STYLE['height']='600px'
 
 def Quadra():
 	Cena_Campo= Cena(img=Campo)
 	Cena_Campo.vai()
+	Elemento_Dogin= Elemento(img=Dogin)    
+	Elemento_Dogin.entra(Cena_Campo)
+    
 Quadra()
