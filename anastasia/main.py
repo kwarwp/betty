c5ddc7d11478d905a4800f51241e1b43a315ba4a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 09:01:58 -0300

    Saved anastasia/main.py @2019-11-19 12:01

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,5 +1,5 @@
 # betty.anastasia.main.py 
-from _spy.vitollino.mean import Cena, Texto, Elemento, STYLE 
+from _spy.vitollino.main import Cena, Texto, Elemento, STYLE 
 espaco="https://conhecimentocientifico.r7.com/wp-content/uploads/2017/02/destaque.jpg"
 ESPACO= Cena ( img=espaco)
 
