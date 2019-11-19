bc8afe9f9da3e6f85de7efc49fdba842e35c6de4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 10:02:18 -0300

    Saved anastasia/main.py @2019-11-19 13:02

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,6 +1,7 @@
 # betty.anastasia.main.py 
 from _spy.vitollino.main import Cena, Texto, Elemento, STYLE 
 espaco="https://conhecimentocientifico.r7.com/wp-content/uploads/2017/02/destaque.jpg"
+nave="http://www.theartofcats.com/gallery2/d/10062-2/nave-espacial-em-png-queroimagem-cei__a-crispim.png"
 
 STYLE["width"]=1100
 STYLE["height"]="800px"
