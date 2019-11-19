9b9f348a88dadef17b23a2ae9e36c24f8c911e06
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 10:55:33 -0300

    Created samantha/__log__.py @2019-11-19 13:55

---
+++ samantha/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Nov 19 2019 10:55:27.336 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  def dragonballz()
+                    ^
+SyntaxError: invalid syntax
+'''},
