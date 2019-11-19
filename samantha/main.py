58e2c621b7bf398cabe8e2bae9c281da88fbbf82
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 10:56:25 -0300

    Created samantha/main.py @2019-11-19 13:56

---
+++ samantha/main.py
@@ -0,0 +1,6 @@
+# betty.samantha.main.py
+from _spy.vitollino.main import Cena,Texto,Elemento,STYLE
+namek='https://images3.alphacoders.com/677/thumb-1920-677271.png'
+def dragonballz():
+	Cena_namek= Cena(img=namek)
+	
