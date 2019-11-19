ef11442d23feace4cf60d75aa24fe620f0c5acdb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 11:15:04 -0300

    Saved samantha/main.py @2019-11-19 14:15

--- samantha/main.py
+++ samantha/main.py
@@ -1,6 +1,7 @@
 # betty.samantha.main.py
 from _spy.vitollino.main import Cena, Texto, Elemento, STYLE
 Namek='https://images3.alphacoders.com/677/thumb-1920-677271.png'
+Battle='https://www.pngfind.com/pngs/m/340-3407741_goku-vs-vegeta-png-dragon-ball-goku-vs.png'
 STYLE['width']=1380
 STYLE['height']='605px'
 
