af10fafa5f912ea9b4704cdf92310864fd6d5da6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 09:58:30 -0300

    Saved anastasia/__log__.py @2019-11-19 12:58

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -168,3 +168,21 @@ SyntaxError: invalid syntax
                                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Nov 19 2019 09:58:27.437 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 13
+    funciona()
+  module <module> line 11
+    	NAVE=Elemento(img=nave, tit="NAVE1",style=dict(left=100, top=250, width="80px", height="90px"))
+NameError: name 'nave' is not defined
+'''},
