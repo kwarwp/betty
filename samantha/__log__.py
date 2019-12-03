f609e754c7d027288e8ede1659970b76e84326be
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-03 09:25:24 -0300

    Saved samantha/__log__.py @2019-12-03 12:25

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -56,3 +56,10 @@ SyntaxError: invalid syntax
                                                                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Dec 03 2019 09:25:26.233 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  Elemento_Battle=Elemento(img=Battle, tit="BATTLE1",style=dict(left=300, top=500, width="126" height="26"))
+                                                                                                ^
+SyntaxError: invalid syntax
+'''},
