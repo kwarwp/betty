453c42da652e82cbe4c56e9b6d9a212178644440
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 09:14:25 -0300

    Saved sarah/main.py @2019-12-04 12:14

--- sarah/main.py
+++ sarah/main.py
@@ -2,6 +2,7 @@
 from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
 praia="https://www.fotoswiki.org/Uploads/fotoswiki.org/ImagensGrandes/praias-7.jpg"
 barco="https://cdn.pixabay.com/photo/2014/04/02/16/23/boat-307125_960_720.png"
+bolsonaro="https://www.pngfind.com/pngs/m/346-3467298_bolsonaro-com-peixe-png-transparent-png.png"
 STYLE["width"]=1280
 STYLE["height"]="670px"
 def foto1():
