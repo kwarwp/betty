f5ffd2bb79da6830f857d127df419c9c402f8a2d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 10:30:59 -0300

    Saved sarah/main.py @2019-12-04 13:30

--- sarah/main.py
+++ sarah/main.py
@@ -1,12 +1,12 @@
 # betty.sarah.main.py
 from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
 naruto="https://uploads.jovemnerd.com.br/wp-content/uploads/2017/03/v-naruto-1210x540.jpg"
-logonaruto="https://logodownload.org/wp-content/uploads/2014/10/naruto-shippuden-logo-1.png"
+logonaruto="https://logodownload.org/wp-content/uploads/2014/10/naruto-logo-shippuden.png"
 STYLE["width"]=1000
 STYLE["height"]="640px"
 def foto1():
 	cenanaruto=Cena(img=naruto)
-	elementologonaruto=Elemento(img=logonaruto,style=dict (top=400,left=300,width="450px",heigth="400px"))
+	elementologonaruto=Elemento(img=logonaruto,style=dict (top=30,left=10,width="450px",heigth="400px"))
 	elementologonaruto.entra(cenanaruto)
 	cenanaruto.vai()
 foto1()
