ea6656cf31c814fcb930f47b24132b4af523fd58
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 09:32:24 -0300

    Saved sarah/main.py @2019-12-04 12:32

--- sarah/main.py
+++ sarah/main.py
@@ -3,6 +3,7 @@ from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
 praia="https://www.fotoswiki.org/Uploads/fotoswiki.org/ImagensGrandes/praias-7.jpg"
 barco="https://cdn.pixabay.com/photo/2014/04/02/16/23/boat-307125_960_720.png"
 bolsonaro="https://pbs.twimg.com/media/CnqKuEnW8AAXMbU.png"
+morinho="https://midianinja.org/files/2018/11/moronu.png"
 STYLE["width"]=1280
 STYLE["height"]="670px"
 def foto1():
@@ -12,4 +13,6 @@ def foto1():
 	elementobarco.entra(cenapraia)
 	elementobolsonaro=Elemento(img=bolsonaro,style=dict (top=350,left=500,width="200px",heigth="100px"))
 	elementobolsonaro.entra(cenapraia)
+	elementomorinho=Elemento(img=morinho,style=dict (top=350,left=500,width="200px",heigth="100px"))
+  
 foto1()
