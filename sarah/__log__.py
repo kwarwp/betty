3dd42a5bb7c7297d64943ec30388521c3a0a0036
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 09:41:25 -0300

    Created sarah/__log__.py @2019-12-04 12:41

---
+++ sarah/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Dec 04 2019 09:41:20.15 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  elementorusso.entra(cenapraia)
+  ^
+IndentationError: unexpected indent
+'''},
