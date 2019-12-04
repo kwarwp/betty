982813bdabce652c79779d3fa33ea4b3c9c8a6cd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 10:24:21 -0300

    Saved sarah/main.py @2019-12-04 13:24

--- sarah/main.py
+++ sarah/main.py
@@ -1,17 +1,12 @@
 # betty.sarah.main.py
 from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
-praia="https://www.fotoswiki.org/Uploads/fotoswiki.org/ImagensGrandes/praias-7.jpg"
-barco="https://cdn.pixabay.com/photo/2014/04/02/16/23/boat-307125_960_720.png"
-bolsonaro="https://pbs.twimg.com/media/CnqKuEnW8AAXMbU.png"
+naruto="https://uploads.jovemnerd.com.br/wp-content/uploads/2017/03/v-naruto-1210x540.jpg"
+logonaruto="https://logodownload.org/wp-content/uploads/2014/10/naruto-shippuden-logo-1.png"
 STYLE["width"]=1280
 STYLE["height"]="670px"
 def foto1():
-	cenapraia=Cena(img=praia)
-	elementobarco=Elemento(img=barco,style=dict (top=400,left=300,width="450px",heigth="400px"))
-	elementobarco.entra(cenapraia)
-	elementobolsonaro=Elemento(img=bolsonaro,style=dict (top=330,left=500,width="130px",heigth="670"))
-	elementobolsonaro.entra(cenapraia)
-	textobolsonaro=Texto(cenapraia,"Comunismo é coisa do lula")
-	elementobolsonaro.vai=textobolsonaro.vai
+	cenanaruto=Cena(img=naruto)
+	elementologonaruto=Elemento(img=logonaruto,style=dict (top=400,left=300,width="450px",heigth="400px"))
+	elementologonaruto.entra(cenapraia)
 	cenapraia.vai()
 foto1()
