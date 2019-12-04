f882c640b360ebcbf4e5df74bab0df4b31e94a96
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 09:41:14 -0300

    Saved sarah/main.py @2019-12-04 12:41

--- sarah/main.py
+++ sarah/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
 praia="https://www.fotoswiki.org/Uploads/fotoswiki.org/ImagensGrandes/praias-7.jpg"
 barco="https://cdn.pixabay.com/photo/2014/04/02/16/23/boat-307125_960_720.png"
 bolsonaro="https://pbs.twimg.com/media/CnqKuEnW8AAXMbU.png"
-morinho="https://midianinja.org/files/2018/11/moronu.png"
+russo="https://logodownload.org/wp-content/uploads/2015/12/Comunismo-Logo-simbolo-8.png"
 STYLE["width"]=1280
 STYLE["height"]="670px"
 def foto1():
@@ -13,6 +13,6 @@ def foto1():
 	elementobarco.entra(cenapraia)
 	elementobolsonaro=Elemento(img=bolsonaro,style=dict (top=350,left=500,width="200px",heigth="100px"))
 	elementobolsonaro.entra(cenapraia)
-	elementomorinho=Elemento(img=morinho,style=dict (top=350,left=500,width="200px",heigth="100px"))
-	elementomorinho.entra(cenapraia)
+	elementorusso=Elemento(img=russo,style=dict (top=1000,left=1000,width="600px",heigth="400px"))
+	 elementorusso.entra(cenapraia)
 foto1()
