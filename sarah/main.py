fd6652721cb2144ebc6ec643869ec5f363e08eaa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 09:02:30 -0300

    Saved sarah/main.py @2019-12-04 12:02

--- sarah/main.py
+++ sarah/main.py
@@ -1,9 +1,12 @@
 # betty.sarah.main.py
 from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
 praia="https://www.fotoswiki.org/Uploads/fotoswiki.org/ImagensGrandes/praias-7.jpg"
+barco="https://cdn.pixabay.com/photo/2014/04/02/16/23/boat-307125_960_720.png"
 STYLE["width"]=1280
 STYLE["height"]="670px"
 def foto1():
 	cenapraia=Cena(img=praia)
 	cenapraia.vai()
+	elementobarco=Elemento(png=barco)
+	elementobarco.entra(cenapraia)
 foto1()
