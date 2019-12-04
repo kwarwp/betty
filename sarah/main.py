ef1fcadb584e40561b5ed8b7488601a557851263
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 10:08:29 -0300

    Saved sarah/main.py @2019-12-04 13:08

--- sarah/main.py
+++ sarah/main.py
@@ -9,7 +9,7 @@ def foto1():
 	cenapraia=Cena(img=praia)
 	elementobarco=Elemento(img=barco,style=dict (top=400,left=300,width="600px",heigth="400px"))
 	elementobarco.entra(cenapraia)
-	elementobolsonaro=Elemento(img=bolsonaro,style=dict (top=350,left=500,width="170px",heigth="430px"))
+	elementobolsonaro=Elemento(img=bolsonaro,style=dict (top=350,left=500,width="170px",heigth="600px"))
 	elementobolsonaro.entra(cenapraia)
 	textobolsonaro=Texto(cenapraia,"Comunismo é coisa do lula")
 	elementobolsonaro.vai=textobolsonaro.vai
