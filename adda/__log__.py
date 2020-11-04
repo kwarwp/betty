1371c091f3900a9217eeacc80a35a34d9f2cf547
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-04 16:14:56 -0300

    Saved adda/__log__.py @2020-11-04 19:14

--- adda/__log__.py
+++ adda/__log__.py
@@ -13,3 +13,26 @@ SyntaxError: invalid syntax
                                                                                                                                                                                                                                                                                                                                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 04 2020 16:14:55.312 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''04/11/2020
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 155
+    tabuleiro = main()
+  module <module> line 150
+    tabuleiro = Tabuleiro()
+  module <module> line 43
+    self.acertos = calcula_casas_alinhadas()
+  module <module> line 39
+    return linha_x + linhas_y + linhas_z
+NameError: name 'linha_x' is not defined
+'''},
