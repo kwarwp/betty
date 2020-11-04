da46acb03acd83c2a5142db386b050fc19b9d8e7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-04 16:13:36 -0300

    Saved adda/__log__.py @2020-11-04 19:13

--- adda/__log__.py
+++ adda/__log__.py
@@ -6,3 +6,10 @@
         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 04 2020 16:13:35.552 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 63
+  return (1<<cor) + (1 << (forma + 6 * (peca//15 + 1) )
+                                                                                                                                                                                                                                                                                                                                                                                      ^
+SyntaxError: invalid syntax
+'''},
