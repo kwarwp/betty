3330d63f9fdffe79d22656cd0d848a6acb93352b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-04 16:13:21 -0300

    Created adda/__log__.py @2020-11-04 19:13

---
+++ adda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Nov 04 2020 16:13:19.166 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 59
+  else 
+        ^
+SyntaxError: invalid syntax
+'''},
