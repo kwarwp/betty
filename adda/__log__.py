dc95e69c7b8509d665803e51142c2e35a3d8d50d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-04 16:15:21 -0300

    Saved adda/__log__.py @2020-11-04 19:15

--- adda/__log__.py
+++ adda/__log__.py
@@ -36,3 +36,24 @@ Traceback (most recent call last):
     return linha_x + linhas_y + linhas_z
 NameError: name 'linha_x' is not defined
 '''},
+{'date': 'Wed Nov 04 2020 16:15:20.942 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''04/11/2020
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 157
+    tabuleiro.atualiza_leitura()
+  module <module> line 107
+    self.valor = self.formata_leitura(novo_valor_leitura)
+  module <module> line 74
+    self.pinos = [self.calcula_propriedade_peca(int(dado)) for dado in linha_lida.split("\t")]
+AttributeError: 'list' object has no attribute 'split'
+'''},
