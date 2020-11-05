ab59ca0a19fede0a6c2f435243eaf6535af0dfcf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-05 16:43:42 -0300

    Saved adda/main.py @2020-11-05 19:43

--- adda/main.py
+++ adda/main.py
@@ -115,7 +115,7 @@ class Tabuleiro():
         cena.height = 600
         self._casas = [Casa(coluna, linha, camada)
                  for coluna in TAM for linha in TAM for camada in TAM]
-        curve((-2,0,0), (2,0,0))
+        curve(pos=[(-4,0,0), (4,0,0)])
 
         
     def calcula_propriedade_peca(self, peca):
