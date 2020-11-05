ff362ab19d998a6aa46ed71af803b8437daca1b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-05 17:10:27 -0300

    Saved adda/__log__.py @2020-11-05 20:10

--- adda/__log__.py
+++ adda/__log__.py
@@ -371,3 +371,23 @@ AttributeError: 'method' object has no attribute '__getitem__'
     Casa.ACASA.append[self]
 AttributeError: 'method' object has no attribute '__getitem__'
 '''},
+{'date': 'Thu Nov 05 2020 17:10:26.494 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 233
+    tabuleiro = main()
+  module <module> line 228
+    tabuleiro = Tabuleiro()
+  module <module> line 124
+    [line(vec(pos_casas(a)), vec(pos_casas(b)), vec(pos_casas(c))) for a, b, c in self.acertos]
+TypeError: 'list' object is not callable
+'''},
