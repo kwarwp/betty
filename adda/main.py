44a4bd6b9a8ccdf048fa0dceaf27554fd4bfcb88
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-05 19:08:28 -0300

    Saved adda/main.py @2020-11-05 22:08

--- adda/main.py
+++ adda/main.py
@@ -96,9 +96,9 @@ class Tabuleiro():
             diag_b_y = [alinhadas for alinhadas in zip(casas[6:9], casas[12:15], casas[18:21])]
             diag_f_z = [alinhadas for alinhadas in zip(casas[::3], casas[10::3], casas[20::3])]
             diag_b_z = [alinhadas for alinhadas in zip(casas[2::3], casas[10::3], casas[18::3])]
-            diag_xyz = [(0,13,26), (2, 13, 24),(5,13,20), (7, 13, 18)]
+            diag_xyz = [(0,13,26), (2, 13, 24),(6,13,20), (8, 13, 18)]
             #diags = diag_f_x +diag_b_x +diag_f_y +diag_b_y +diag_f_z +diag_b_z+diag_xyz
-            diags = diag_f_z +diag_b_z
+            diags = diag_xyz
             # print(diags)
             # return linhas_x + linhas_y + linhas_z + diags
             return diags
