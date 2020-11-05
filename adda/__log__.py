5cf1fb1c606c0a792469f118ecfbdbe5cb3cbc0e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-05 19:26:05 -0300

    Saved adda/__log__.py @2020-11-05 22:26

--- adda/__log__.py
+++ adda/__log__.py
@@ -455,3 +455,19 @@ Exception: <SyntaxError: Unexpected token ','>
                                                                                                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Nov 05 2020 19:26:04.385 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    FORMA = ((box,L,Z), (box,S,Z), (cylinder,L,Z), (cylinder,S,Z), (cylinder,L,S))
+NameError: name 'L' is not defined
+'''},
