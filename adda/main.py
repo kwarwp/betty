d2da12f3f8a0b05b34bb3733cfa10448ca5f4c2b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-05 20:50:19 -0300

    Saved adda/main.py @2020-11-05 23:50

--- adda/main.py
+++ adda/main.py
@@ -160,7 +160,7 @@ class Tabuleiro():
         #[line(vec(*pos_casas[a]), vec(*pos_casas[b]), vec(*pos_casas[c])) for a, b, c in self.acertos]
         #Cubo(-2,0,0,self.calcula_propriedade_peca(20))
         [Cubo(-2, y, z, self.calcula_propriedade_peca(tipo+11))
-            for tipo, (y, z) tipo in enumerate((y,z) for y in (-1,0,1) for z in (-2,-1,0,1,2))]
+            for tipo, (y, z) in enumerate((y,z) for y in (-1,0,1) for z in (-2,-1,0,1,2))]
 
         
     def calcula_propriedade_peca(self, peca):
