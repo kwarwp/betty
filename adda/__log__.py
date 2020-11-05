135227418a3815dbc02f2ce61b0f79d73f8694ad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-05 16:33:05 -0300

    Saved adda/__log__.py @2020-11-05 19:33

--- adda/__log__.py
+++ adda/__log__.py
@@ -273,3 +273,23 @@ Exception: <SyntaxError: Unexpected token ','>
     curve((-2,0,0), (2,0,0))
 TypeError: __init__() takes 1 positional arguments but more were given
 '''},
+{'date': 'Thu Nov 05 2020 16:33:03.726 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 227
+    tabuleiro = main()
+  module <module> line 222
+    tabuleiro = Tabuleiro()
+  module <module> line 118
+    curve(vec(-2,0,0), vec(2,0,0))
+TypeError: __init__() takes 1 positional arguments but more were given
+'''},
