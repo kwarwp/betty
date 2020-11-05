62a4a16e97aa9a31b9e2d1548321eb16c88bb7b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-05 17:09:01 -0300

    Saved adda/__log__.py @2020-11-05 20:09

--- adda/__log__.py
+++ adda/__log__.py
@@ -349,3 +349,25 @@ Exception: <Error: A curve object point must include a pos.>
     Casa.ACASA.append[self]
 AttributeError: 'method' object has no attribute '__getitem__'
 '''},
+{'date': 'Thu Nov 05 2020 17:09:00.373 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 233
+    tabuleiro = main()
+  module <module> line 228
+    tabuleiro = Tabuleiro()
+  module <module> line 120
+    self._casas = [Casa(coluna, linha, camada)
+  module <module> line 51
+    Casa.ACASA.append[self]
+AttributeError: 'method' object has no attribute '__getitem__'
+'''},
