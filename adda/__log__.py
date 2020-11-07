e6eb19269217e519cf9fe71b8698f8394ea45123
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-07 17:28:05 -0300

    Saved adda/__log__.py @2020-11-07 20:28

--- adda/__log__.py
+++ adda/__log__.py
@@ -703,3 +703,27 @@ KeyError: 64
     forma, gran, peq, des = FORMAS[tipo >> 6]
 KeyError: 64
 '''},
+{'date': 'Sat Nov 07 2020 17:28:04.866 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 274
+    tabuleiro = main()
+  module <module> line 269
+    tabuleiro = Tabuleiro()
+  module <module> line 142
+    self.casas()
+  module <module> line 164
+    [Cubo(2, y, z, self.calcula_propriedade_peca(tipo+26))
+  module <module> line 56
+    forma, gran, peq, des = FORMAS[tipo >> 6]
+KeyError: 1024
+'''},
