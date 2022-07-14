7aeac90f716310d9bc92a63696d3c751f5a29882
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-14 09:17:39 -0300

    Saved tracy/__log__.py @2022-07-14 12:17

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -913,3 +913,25 @@ AttributeError: 'Cena' object has no attribute 'cenario'
     	cenario2.esquerda = cenario2.cenario
 AttributeError: 'Cena' object has no attribute 'cenario'
 '''},
+{'date': 'Thu Jul 14 2022 09:17:38.405 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 98
+    Incrivel_banana_python()
+  module <module> line 27
+    	elemento = Elemento(img = banana ,
+  module elemento.main line 19
+    self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
+  module _spy.vitollino.main line 668
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
